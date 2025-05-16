5137ff083ec3cc5b1b937230c736f069c88703b4
RandDouble <stefano.pilosio@gmail.com>
2025-05-16 18:16:44 +0200

    Added a few math functions

--- C/DoubleConversionToFPX.c
+++ C/DoubleConversionToFPX.c
@@ -1,72 +1,135 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdint.h>
 
 #define M_SIZE 32
 #define E_SIZE 32
 
-typedef struct {
-  char s;
-  char m[M_SIZE];
-  char e[E_SIZE];
+typedef struct
+{
+  char s;         // Segno vale '0' o '1'
+  char m[M_SIZE]; // Mantissa definita come stringa
+  char e[E_SIZE]; // Esponente definito come stringa '010' = 2^2
 } FPX;
 
-//Conversione Binario -> Decimale
-int binaryToInteger(const char*);
+// Conversione Binario -> Decimale
+int binaryToInteger(const char *);
 
-//Conversione Decimale -> Binario
-void integerToBinary(int, char*);
+// Conversione Decimale -> Binario
+void integerToBinary(int, char *);
 
-//Conversione Double -> FPX
+// Conversione Double -> FPX
 FPX doubleToFPX(double);
 
-//Conversione FPX -> Double
-double FPXToDouble(const FPX*);
+// Conversione FPX -> Double
+double FPXToDouble(const FPX *);
 
-//Prodotto tra FPX
-FPX FPXProduct(const FPX*, const FPX*);
+// Prodotto tra FPX
+FPX FPXProduct(const FPX *, const FPX *);
 
-//Somma tra FPX
-FPX FPXSum(const FPX*, const FPX*);
+void BitVectorAdd(const char *, const char *, char *);
+void BitVectorProd(const char*, const char *, char *);
 
-int main(int argc, char** argv) {
+
+// Somma tra FPX
+FPX FPXSum(const FPX *, const FPX *);
+
+int main(int argc, char **argv)
+{
 
   return 0;
 }
 
-//Conversione Binario -> Decimale
-int binaryToInteger(const char* input) {
-  int output;
+// Conversione Binario -> Decimale
+int binaryToInteger(const char *input)
+{
+  int output = 0;
+
+  while (*input != '\0')
+  {
+    output <<= 1;
+    output += (*input == '0') ? 0 : 1;
+    input++;
+  }
 
   return output;
 }
 
-//Conversione Decimale -> Binario
-void integerToBinary(int input, char* output) {
+// Conversione Decimale -> Binario
+void integerToBinary(int input, char *output)
+{
+  // Voglio che abbia la dimensione di un intero * la dimensione di un byte,
+  // non so quanto fosse grande in precedenza, quindi lo riassegno
+  output = (char *)realloc(sizeof(int) * 8);
 
+  if (output == NULL)
+  {
+    printf("Memory allocation failed\n");
+    return;
+  }
+
+  for (int i = 0; i < sizeof(int) * 8; i++)
+  {
+    output[sizeof(int) * 8 - 1 - i] = (input & 1) ? '1' : '0';
+    input >>= 1;
+  }
+
+  // Al termine della conversione l'output deve essere o 0 o 1
+  // Se non è così, la conversione non è riuscita
+  assert((input == 0 || input == 1) && "Conversione non riuscita");
 }
 
-//Conversione Double -> FPX
-FPX doubleToFPX(double input) {
-  FPX output;
+// Conversione Double -> FPX
+FPX doubleToFPX(double input)
+{
+  int mantissa = 0, exponent = 0;
+  FPX output = {.s = (input > 0) ? '1' : '0', .m = NULL, .e = NULL};
+
+  int64_t input_bits = *((int64_t *)&input);
+
+  mantissa = input_bits & 0x000FFFFFFFFFFFFF;         // Mantissa
+  exponent = (input_bits & 0xEFF0000000000000) >> 52; // Esponente
+
+  integerToBinary( mantissa , output.m );
+  integerToBinary( exponent , output.e );
 
   return output;
 }
 
-//Conversione FPX -> Double
-double FPXToDouble(const FPX* input) {
+// Conversione FPX -> Double
+double FPXToDouble(const FPX *input)
+{
   double output;
 
   return output;
 }
 
-//Prodotto tra FPX
-FPX FPXProduct(const FPX* inputA, const FPX* inputB) {
+// Prodotto tra FPX
+FPX FPXProduct(const FPX *inputA, const FPX *inputB)
+{
   FPX output;
+
 
   return output;
 }
 
-//Somma tra FPX
-FPX FPXSum(const FPX* inputA, const FPX* inputB) {
+void BitVectorAdd(const char *first, const char *second, char *out)
+{
+  // Implementing Carry Addition
+  char carry = '0';
+  
+
+  return;
+}
+
+void BitVectorProd(const char *first, const char *second, char *out)
+{
+  return;
+}
+
+// Somma tra FPX
+FPX FPXSum(const FPX *inputA, const FPX *inputB)
+{
   FPX output;
 
   return output;
