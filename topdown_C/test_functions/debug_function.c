153c15d89a84f2f0a54d99893a93360be4d5a6c7
RandDouble <stefano.pilosio@gmail.com>
2025-06-05 12:46:50 +0200

    Added printing function to hexadecimal

--- topdown_C/test_functions/debug_function.c
+++ topdown_C/test_functions/debug_function.c
@@ -17,6 +17,22 @@ void print_uint64_bin(const uint64_t value) {
     return;
 }
 
+void print_uint128_hex(const __uint128_t value)
+{
+    for (int i = 15; i >= 0; i--) {
+        printf("%01x", (unsigned int)(value >> (i * 8)) & 0xFF);
+    }
+    putchar('\n');
+}
+
+void print_uint64_hex(const uint64_t value)
+{
+    for (int i = 7; i >= 0; i--) {
+        printf("%01x", (unsigned int)(value >> (i * 8)) & 0xFF);
+    }
+    putchar('\n');
+}
+
 void print_bin(const void* value, print_bin_mode_t mode) {
     switch (mode) {
         case UINT128:
@@ -25,6 +41,21 @@ void print_bin(const void* value, print_bin_mode_t mode) {
         case UINT64:
           print_uint64_bin(*((uint64_t*) value));
         break;
+        default:
+            assert(mode == UINT64 && "mode selection undefined");
+        break;
+    }
+    return;
+}
+
+void print_hex(const void* value, print_bin_mode_t mode) {
+    switch (mode) {
+        case UINT128:
+          print_uint128_bin(*((__uint128_t*) value));
+          return;
+        case UINT64:
+          print_uint64_bin(*((uint64_t*) value));
+          return;
         default:
             assert(mode == UINT64 && "mode selection undefined");
         break;
